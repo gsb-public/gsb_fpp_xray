9fcc032b91063a38502635e931ff8ae74790ef8a
Greg Mercer (GBear) <gmercer@stanford.edu>
2013-10-30 10:08:56 -0700

    WP-2288 Changes to use the temp dir for downloading csv xray files

--- gsb_fpp_xray_form_data.tpl.php
+++ gsb_fpp_xray_form_data.tpl.php
@@ -1,6 +1,6 @@
 <?php //dpm($variables); ?>
 
 <?php if (!empty($variables['fpid']) || !empty($variables['bundle'])) { ?>
-	<div>Download your csv formatted output <a href="/info.csv">here</a>.</div>
+	<div>Download your csv formatted output <a href="/admin/config/gsb/fpp-xray/download/<?php print $variables['file_name'] ?>.csv">here</a>.</div>
 <?php } ?>
 
